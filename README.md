92f4a7a40bac1bc2057c1a4d0682a38667727220
lifeng87 <oscarcoll.930714@gmail.com>
2022-05-20 23:47:25 +0800

    readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Pixiv 登入系統練習
 練習網路爬蟲用的專案
 ## 目前問題
-因為反爬的關係無法取得recaptcha-token
+```
+因為反爬的關係無法取得recaptcha-token (OK)
+解決方法: 不做登入直接在header傳入referer就行了
+```
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ getTop();
 async function getTop(){
     const res = await axios({
         method:"get",
-        url:"https://www.pixiv.net/ranking.php",
+        url:"https://www.pixiv.net/ranking.php?mode=daily&content=illust",
         headers:{
             "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47"
         }
