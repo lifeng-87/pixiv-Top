4077e4d2650fb6816828fcf61266ea7b8c0f4e53
lifeng87 <oscarcoll.930714@gmail.com>
2022-05-20 21:46:37 +0800

    :(

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ async function getTop(){
     getImg(originImgUrl);
 }
 
+async function getImg(imgUrl){
+    const res = await axios({
+        method:"get",
+        url:imgUrl,
+        headers:{
+            "referer": "http://www.pixiv.net/",
+            "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47"
+        }
+    })
+}
+
 function delay(ms){
     return new Promise((r)=>setTimeout(r, ms))
 }
